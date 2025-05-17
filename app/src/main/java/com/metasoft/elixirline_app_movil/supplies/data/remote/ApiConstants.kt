e6a7e3db8861bd2f9a112b18f964ae75790e764f
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2025-05-17 04:50:18 -0500

    feat: add ApiConstants.kt for json server

---
+++ app/src/main/java/com/metasoft/elixirline_app_movil/supplies/data/remote/ApiConstants.kt
@@ -0,0 +1,7 @@
+package com.metasoft.elixirline_app_movil.supplies.data.remote
+
+object ApiConstants {
+    const val BASE_URL = "https://my-json-server.typicode.com/elixirline/api/"
+    const val SUPPLIES_ENDPOINT = "supplies"
+    const val SUPPLY_USAGE_ENDPOINT = "supply_usage"
+}
