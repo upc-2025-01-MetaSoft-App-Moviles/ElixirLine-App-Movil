baf27a4fd8abb0b9684e1d56613a641d8b63ba63
Fabricio Apaza Morales <josueapaza01@gmail.com>
2025-05-16 19:23:10 -0500

    fix(ProductionHistory): added new color for NavBar

--- app/src/main/java/com/metasoft/elixirline_app_movil/presentation/navigation/Home.kt
+++ app/src/main/java/com/metasoft/elixirline_app_movil/presentation/navigation/Home.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
@@ -34,7 +35,9 @@ fun Home() {
 
     Scaffold (
         bottomBar = {
-            BottomAppBar {
+            BottomAppBar(
+                containerColor = Color(0xFF8B0000)
+            ) {
                 navigationItems.forEachIndexed { index, item ->
                     NavigationBarItem(
                         selected = index == selectedIndex.value,
