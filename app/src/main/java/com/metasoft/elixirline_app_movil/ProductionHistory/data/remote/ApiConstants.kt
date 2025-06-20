d7ea1d3adef54ea53356a07f2c72fffe809a5381
Fabricio <115115308+Fabricio011001@users.noreply.github.com>
2025-06-20 16:48:36 -0500

    feat(production-history): added api deployed

--- app/src/main/java/com/metasoft/elixirline_app_movil/ProductionHistory/data/remote/ApiConstants.kt
+++ app/src/main/java/com/metasoft/elixirline_app_movil/ProductionHistory/data/remote/ApiConstants.kt
@@ -3,7 +3,7 @@ package com.metasoft.elixirline_app_movil.ProductionHistory.data.remote
 object ApiConstants {
     const val FAKE_API_URL = "https://my-json-server.typicode.com/fabricio011001/fakeapi/"
 
-    const val REAL_API_URL = "http://10.0.2.2:5110"
+    const val REAL_API_URL = "https://elixirline.azurewebsites.net/"
 
     const val BASE_URL = REAL_API_URL
 }
