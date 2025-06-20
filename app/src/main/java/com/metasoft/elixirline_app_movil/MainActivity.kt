45598c3fdce03d4b10ffe7f8b00efcaefa00a433
Janover Saldaña <u20201b510@upc.edu.pe>
2025-06-19 20:06:28 -0500

    feat(merge-winemaking): The winemaking process branch was integrated into the development branch.

--- app/src/main/java/com/metasoft/elixirline_app_movil/MainActivity.kt
+++ app/src/main/java/com/metasoft/elixirline_app_movil/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.metasoft.elixirline_app_movil.ui.theme.ElixirLineAppMovilTheme
+import com.metasoft.elixirline_app_movil.winemakingprocess.presentation.navigation.Home
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +14,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ElixirLineAppMovilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Home()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ElixirLineAppMovilTheme {
-        Greeting("Android")
-    }
-}
